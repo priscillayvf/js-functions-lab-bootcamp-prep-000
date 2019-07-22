bad80b7dd94cc6b367736ad794d908f49881582a
Priscilla Firpo <priscillayf@gmail.com>
2019-07-22 02:20:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+function happyHolidayto(holiday, name)
+  return `Happy ${holiday}, ${name}!`
+}
